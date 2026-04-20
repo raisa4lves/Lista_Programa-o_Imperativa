b9fa26b38503d393df8f3e58f5cb238270ea14f5
raisa4lves <raisaalves9872@gmail.com>
2026-04-19 22:00:59 -0300

    Update Q4.java

--- Q4.java
+++ Q4.java
@@ -1,1 +1,37 @@
+import java.util.Scanner;
 
+public class Q4 {
+
+    public static int buscar(int[] v, int x) {
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] codigos = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            codigos[i] = sc.nextInt();
+        }
+
+        int x = sc.nextInt();
+
+        int pos = buscar(codigos, x);
+
+        if (pos != -1) {
+            System.out.println("Codigo encontrado");
+            System.out.println("Primeira posicao: " + pos);
+        } else {
+            System.out.println("Codigo nao encontrado");
+        }
+
+        sc.close();
+    }
+}
